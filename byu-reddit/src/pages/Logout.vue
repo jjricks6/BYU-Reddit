b9928039da2954a3c58b5eb2522b0dbe3fa94cca
jjricks6 <jjricks1@gmail.com>
2022-03-03 20:53:07 -0700

    Base project

---
+++ byu-reddit/src/pages/Logout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Logging you out...</div>
+</template>
+
+<script>
+import { deleteJwtToken } from "../auth";
+export default {
+  name: "LogoutPage",
+  mounted: function () {
+    deleteJwtToken();
+    this.$router.push({ path: "/" });
+  },
+};
+</script>
